afc484985835a3da773b5d3b4e6caddec5c39f3a
Raja Indra Ramoza <56436175+rajaindra97@users.noreply.github.com>
2021-11-11 17:41:36 +0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,75 @@
+// creating an array and passing the number, questions, options, and answers
+
+let questions = [
+    {
+    numb: 1,
+    question: "<img src='gambar.jpg' width='220' height='190'/>",
+    answer: 0,
+    options: [
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>",
+      "<img src='gambar.jpg' width='120' height='90'/>",
+      // "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "<img src='gambar.jpg' width='220' height='190'/>",
+    answer: 3,
+    options: [
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>",
+      "<img src='gambar.jpg' width='120' height='90'/>",
+    ]
+  },
+    {
+    numb: 3,
+    question: "<img src='gambar.jpg' width='220' height='190'/>",
+    answer: 0,
+    options: [
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>",
+      "<img src='gambar.jpg' width='120' height='90'/>",
+
+    ]
+  },
+    {
+    numb: 4,
+    question: "<img src='gambar.jpg' width='220' height='190'/>",
+    answer: 3,
+    options: [
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>",
+      "<img src='gambar.jpg' width='120' height='90'/>",
+    ]
+  },
+    {
+    numb: 5,
+    question: "<img src='gambar.jpg' width='220' height='190'/>",
+    answer: 0,
+    options: [
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>" ,
+      "<img src='gambar.jpg' width='120' height='90'/>",
+      "<img src='gambar.jpg' width='120' height='90'/>",
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
